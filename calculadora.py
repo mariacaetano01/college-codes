f335c4b650e15f14582cbb199a7532ee27e059c4
Maria Fernanda Caetano <103069653+mariacaetano01@users.noreply.github.com>
2022-07-16 15:56:43 -0300

    código em processo ☻

--- calculadora.py
+++ calculadora.py
@@ -17,15 +17,15 @@ seu primeeiro e último nome (Tabela registrada no Clasroom)
     • E = 11.18
 
 SAÍDAS
-    • A equação formada, conforme orientado na tabela OK
-    • Os intervalos da primeira etapa do método
+    • A equação formada, conforme orientado na tabela #OK
+    • Os intervalos da primeira etapa do método #OK
     • As soluções encontradas e asociadas a cada intervalo
     • O gráfico da iteração vs. o erro |xi-x(i-1)|
 '''
 
-
 def contador(inicial, final, incremento):
     # O contador vai definir um intervalo de x para calcular y
+
     contador = 0
     while inicial <= final:
         contador += 1
@@ -33,9 +33,9 @@ def contador(inicial, final, incremento):
     return contador
 
 
-def identificador(inicial, contador, incremento):
-    # O identificador consiste em encontrar mudança de sinais entre os
-    # resultados da equação
+def valores_de_y(inicial, contador, incremento):
+    # Os valores de y representam os valores de f(x) a serem
+    # usados no identificador
 
     lista_y = []
     lista_x = []
@@ -47,28 +47,36 @@ def identificador(inicial, contador, incremento):
         lista_y.append(y)
         lista_x.append(x)
         x += incremento
+    return lista_y
 
+
+def identificador(lista_y):
+    # O identificador consiste em encontrar mudança de sinais entre os
+    # resultados da equação
+
+    lista_valores_y = lista_y
     intervalos_1 = []
     intervalos_2 = []
-    for pos,_ in enumerate(lista_y):
-        if (lista_y[pos] < 0 and lista_y[(pos-1)] > 0) or (lista_y[pos] > 0 and lista_y[(pos-1)] < 0):
-            intervalos_1.append(lista_y[pos-1])
-            intervalos_2.append(lista_y[pos])
+    for pos, _ in enumerate(lista_valores_y):
+        if (lista_valores_y[pos] < 0 and lista_valores_y[(pos-1)] > 0) or (lista_valores_y[pos] > 0 and lista_valores_y[(pos-1)] < 0):
+            intervalos_1.append(lista_valores_y[pos-1])
+            intervalos_2.append(lista_valores_y[pos])
 
-    intervalos = zip(intervalos_1,intervalos_2)
+    del intervalos_1[0]
+    del intervalos_2[0]
+    intervalos = zip(intervalos_1, intervalos_2)
     return list(intervalos)
 
 
-def refinador():
+def refinador(intervalos):
     # O refinador consiste em diminuir cada intervalo até atender o
     # critério de convergência e encontrar a aproximação da raiz da
     # equação
-
-    return 'ERRO'
+    return 'Ainda não esá pronto'
 
 
 def main():
-    print('Equação Polinomial: MARIA CAETANO')
+    print('EQUAÇÃO POLINOMIAL: MARIA CAETANO')
     print('y = (a*(x^5)) + (b*(x^4)) + (c*(x^3)) + (d*(x^2)) + (e*x) + f')
     print('y = (1,65*(x^5)) + (-3,510*(x^4)) + (0,571*(x^3))+ (-17,1*(x^2)) + (-3,510*x) + 11,18')
     print('')
@@ -78,9 +86,13 @@ def main():
     incremento = 0.5
     intervalo_de_contagem = contador(valor_inicial, valor_final, incremento)
 
-    y = identificador(valor_inicial, intervalo_de_contagem, incremento)
-    print(y)
+    valores = valores_de_y(valor_inicial, intervalo_de_contagem, incremento)
+    identifica = identificador(valores)
+    print('INTERVALOS COM MUDANÇA DE SINAL:')
+    print(identifica)
+    print(' ')
 
 
 if __name__ == "__main__":
     main()
+
